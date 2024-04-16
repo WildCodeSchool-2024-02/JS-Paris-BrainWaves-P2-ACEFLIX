1c0186af17a9287004b20e954fb409dae8cc23d7
Anthony DUFRENOT <anthony.dufrenot@gmail.com>
2024-04-16 16:47:40 +0200

    slider popular page d'accueil

---
+++ client/src/components/Popular/Popular.jsx
@@ -0,0 +1,109 @@
+/* eslint-disable import/no-unresolved */
+import PropTypes from "prop-types";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { FreeMode, Navigation } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/scrollbar";
+import "./popular.css";
+import useFetch from "../../useFetch";
+import Card from "../Card/Card";
+
+export default function Popular({ status, uniqueTendances, shuffle }) {
+  // URL des Movies et Series tendances
+  const tendancesMoviesFetchURL =
+    "https://api.themoviedb.org/3/movie/popular?language=en-US&page=1&api_key=aea07ae608264c18c1ea1431604753c3";
+  const tendancesSeriesFetchURL =
+    "https://api.themoviedb.org/3/tv/popular?language=en-US&page=1&api_key=aea07ae608264c18c1ea1431604753c3";
+
+  // Fetch de ces contenus via le Hook useFetch (20 de chaque)
+  const {
+    data: tendancesMovies,
+    loading: loadingTendancesMovies,
+    error: errorTendancesMovies,
+  } = useFetch(tendancesMoviesFetchURL);
+  const {
+    data: tendancesSeries,
+    loading: loadingTendancesSeries,
+    error: errorTendancesSeries,
+  } = useFetch(tendancesSeriesFetchURL);
+
+  // Fonction permettant de mélanger 2 tableaux après les avoir concaténés
+  const ShuffleConcat = (arr1, arr2) => {
+    const final = shuffle(arr1.concat(arr2));
+    return final;
+  };
+
+  // Fusion + mélange des tendances movies + series et limité à 15
+  let allTendances = [];
+  if (tendancesMovies && tendancesSeries) {
+    allTendances = ShuffleConcat(tendancesMovies, tendancesSeries).slice(0, 15);
+  }
+  if (loadingTendancesMovies || loadingTendancesSeries) {
+    return <h1>LOADING ...</h1>;
+  }
+  if (errorTendancesMovies || errorTendancesSeries) {
+    console.info("Error");
+  }
+
+  return (
+    <div>
+      <h1 className="main-title">POPULAR</h1>
+      <div className="slider-container">
+        <Swiper
+          modules={[Navigation, FreeMode]}
+          spaceBetween={5}
+          slidesPerView={6}
+          // eslint-disable-next-line react/jsx-boolean-value
+          loop={true}
+          // eslint-disable-next-line react/jsx-boolean-value
+          freeMode={true}
+          breakpoints={{
+            1200: {
+              slidesPerView: 6,
+              spaceBetween: 5,
+            },
+            750: {
+              slidesPerView: 5,
+              spaceBetween: 5,
+            },
+            500: {
+              slidesPerView: 4,
+              spaceBetween: 5,
+            },
+            320: {
+              slidesPerView: 3,
+              spaceBetween: 5,
+            },
+            280: {
+              slidesPerView: 2,
+              spaceBetween: 5,
+            },
+          }}
+          navigation
+          className="mySwiper"
+        >
+          <div>
+            {status
+              ? uniqueTendances?.map((content) => (
+                  <SwiperSlide key={content.id}>
+                    <Card image={content.poster_path} id={content.id} />
+                  </SwiperSlide>
+                ))
+              : allTendances?.map((content) => (
+                  <SwiperSlide key={content.id}>
+                    <Card image={content.poster_path} id={content.id} />
+                  </SwiperSlide>
+                ))}
+          </div>
+        </Swiper>
+      </div>
+    </div>
+  );
+}
+
+Popular.propTypes = {
+  status: PropTypes.bool.isRequired,
+  uniqueTendances: PropTypes.oneOfType([PropTypes.array.isRequired]).isRequired,
+  shuffle: PropTypes.func.isRequired,
+};
--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,24 +1,39 @@
 import { useState } from "react";
 import Top10 from "../../components/Top10/Top10";
-import './home.css'
+import "./home.css";
+import Popular from "../../components/Popular/Popular";
 
 export default function Home() {
   // Initialisation des states
   const [status, setStatus] = useState(false); // State qui donne info si l'utilisateur à cliquer sur films ou séries
   const [uniqueTop, setUniqueTop] = useState([]); // State qui vient ajouter les données d'une seule catégorie
+  const [uniqueTendances, setUniqueTendances] = useState([]); // State qui vient ajouter les données d'une seule catégorie
 
   // URL à fetch
   const moviesFetch =
     "https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1&api_key=aea07ae608264c18c1ea1431604753c3";
   const seriesFetch =
     "https://api.themoviedb.org/3/tv/top_rated?language=en-US&page=1&api_key=aea07ae608264c18c1ea1431604753c3";
+  const popularSeries =
+    "https://api.themoviedb.org/3/tv/popular?language=en-US&page=1&api_key=aea07ae608264c18c1ea1431604753c3";
+  const popularMovies =
+    "https://api.themoviedb.org/3/movie/popular?language=en-US&page=1&api_key=aea07ae608264c18c1ea1431604753c3";
 
   // Comportements
+
+  const shuffle = (arr) => [...arr].sort(() => Math.random() - 0.5);
+
   const handleMovies = () => {
     setStatus(true);
     fetch(moviesFetch)
       .then((response) => response.json())
       .then((response) => setUniqueTop(response.results.splice(0, 10)))
+      .catch((err) => console.error(err));
+    fetch(popularMovies)
+      .then((response) => response.json())
+      .then((response) =>
+        setUniqueTendances(shuffle(response.results).splice(0, 15))
+      )
       .catch((err) => console.error(err));
   };
 
@@ -27,6 +42,12 @@ export default function Home() {
     fetch(seriesFetch)
       .then((response) => response.json())
       .then((response) => setUniqueTop(response.results.splice(0, 10)))
+      .catch((err) => console.error(err));
+    fetch(popularSeries)
+      .then((response) => response.json())
+      .then((response) =>
+        setUniqueTendances(shuffle(response.results).splice(0, 15))
+      )
       .catch((err) => console.error(err));
   };
 
@@ -48,6 +69,11 @@ export default function Home() {
         </button>
       </div>
       <Top10 status={status} uniqueTop={uniqueTop} />
+      <Popular
+        status={status}
+        uniqueTendances={uniqueTendances}
+        shuffle={shuffle}
+      />
     </>
   );
 }
