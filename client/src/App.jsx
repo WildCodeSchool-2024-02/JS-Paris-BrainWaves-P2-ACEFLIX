e7e24812dfe9bcbb2d8fae6f9242be7d9e9af004
Anthony DUFRENOT <anthony.dufrenot@gmail.com>
2024-04-16 16:22:17 +0200

    slider top 10 page d'accueil

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,13 @@
+import { Outlet } from "react-router-dom";
 import "./App.css";
 
 function App() {
-
   return (
-    <h1>Bonjour</h1>
-  )
+    <>
+      <h1>Bonjour</h1>
+      <Outlet />
+    </>
+  );
 }
 
 export default App;
