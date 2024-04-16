7e002ead31529925677e811874a75c908fe93a5a
Anthony DUFRENOT <anthony.dufrenot@gmail.com>
2024-04-16 10:12:56 +0200

    setup projet prêt

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,12 +3,35 @@ import ReactDOM from "react-dom/client";
 
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
+import Home from './pages/Home/Home'
+import Filter from './pages/Filter/Filter'
+import Final from './pages/Final/Final'
 import App from "./App";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/filter/:type",
+        element: <Filter />,
+      },
+      {
+        path: "/final/:id",
+        element: <Final />,
+      },
+      {
+        path: "/search/:input",
+        element: <Final />,
+      }
+
+    ]
+
   },
 ]);
 
