6d9f9ca8f06abdfbb45e3006806f7bafd9a328f9
Erwan Bataille <erwan.bataille2910@gmail.com>
2024-04-19 10:57:38 +0200

    fin nav bar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,15 @@
+import { useState } from "react";
 import { Outlet } from "react-router-dom";
 import "./App.css";
 import Nav from "./components/Nav/Nav";
+import BlackScreen from "./components/BlackScreen/BlackScreen";
 
 function App() {
+  const [isOpen, setIsOpen] = useState(true);
   return (
     <div id="app">
-      <Nav />
+      <Nav setIsOpen={setIsOpen} isOpen={isOpen} />
+      {isOpen && <BlackScreen setIsOpen={setIsOpen} />}
       <Outlet />
     </div>
   );
--- client/src/components/Nav/Nav.jsx
+++ client/src/components/Nav/Nav.jsx
@@ -1,26 +1,69 @@
-import "./nav.css";
+/* eslint-disable import/no-duplicates */
+import PropTypes from "prop-types";
 import { useNavigate } from "react-router-dom";
+import { RxCross2 } from "react-icons/rx";
+import { IoHomeOutline } from "react-icons/io5";
+import { BiCameraMovie } from "react-icons/bi";
+import { MdOutlineAccountCircle } from "react-icons/md";
+import { BiMoviePlay } from "react-icons/bi";
+import { CiMenuBurger } from "react-icons/ci";
+import "./nav.css";
 
-export default function Nav() {
+export default function Nav({ setIsOpen, isOpen }) {
   const navigate = useNavigate();
 
+  const closeNav = () => {
+    setIsOpen(false);
+  };
+
+  const openNav = () => {
+    setIsOpen(true);
+  };
+
   return (
-    <nav>
+    <nav className={isOpen ? "open" : ""}>
+      <div className="icon">
+        <RxCross2 className="close-nav" onClick={closeNav} />
+        <CiMenuBurger onClick={openNav} />
+      </div>
       <ul>
-        <li onClick={() => navigate("/")} role="presentation">
-          Home
+        <li
+          className="list-nav-elements"
+          onClick={() => navigate("/")}
+          role="presentation"
+        >
+          <IoHomeOutline className="icon-nav" />
+          <p>Home</p>
         </li>
-        <li onClick={() => navigate("/filter/movie")} role="presentation">
-          Movies
+        <li
+          className="list-nav-elements"
+          onClick={() => navigate("/filter/movie")}
+          role="presentation"
+        >
+          <BiMoviePlay className="icon-nav" />
+          <p>Movies</p>
         </li>
-        <li onClick={() => navigate("/filter/tv")} role="presentation">
-          Series
+        <li
+          className="list-nav-elements"
+          onClick={() => navigate("/filter/tv")}
+          role="presentation"
+        >
+          <BiCameraMovie className="icon-nav" />
+          <p>Series</p>
         </li>
-        <li>Account</li>
+        <li className="list-nav-elements">
+          <MdOutlineAccountCircle className="icon-nav" />
+          <p>Account</p>
+        </li>
       </ul>
       <div className="logo">
-        <img id="Logo" src="./src/assets/images/Logo.png" alt="Logo" />
+        <img id="logo" src="./src/assets/images/Logo.png" alt="Logo" />
       </div>
     </nav>
   );
 }
+
+Nav.propTypes = {
+  setIsOpen: PropTypes.func.isRequired,
+  isOpen: PropTypes.bool.isRequired,
+};
