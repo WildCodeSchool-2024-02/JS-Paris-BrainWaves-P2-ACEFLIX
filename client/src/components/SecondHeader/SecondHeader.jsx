37e91f251969196d9dbb29cc49e5053a87e0c9d4
Curtisdak <curtis.dakouri@gmail.com>
2024-04-22 15:52:40 +0200

    Action slider is done

--- client/src/components/SecondHeader/SecondHeader.jsx
+++ client/src/components/SecondHeader/SecondHeader.jsx
@@ -1,17 +1,26 @@
 import "./secondHeader.css";
+import PropTypes from "prop-types";
 
-export default function SecondHeader() {
+export default function SecondHeader({ handleAll, handleMovies, handleSeries}) {
+  
   return (
     <div id="SecondHeader">
       <div className="SecondHeader-btn">
-        <button type="button" value="movie">
+        <button type="button" value="movie" onClick={handleMovies}>
           Movies
         </button>
-        <button type="button">All</button>
-        <button type="button" value="tv_serie">
+        <button type="button" onClick={handleAll}>All</button>
+        <button type="button" value="tv_serie"  onClick={handleSeries}> 
           Series
         </button>
       </div>
     </div>
   );
 }
+
+SecondHeader.propTypes = {
+  handleMovies: PropTypes.func.isRequired,
+  handleAll: PropTypes.func.isRequired,
+  handleSeries: PropTypes.func.isRequired,
+};
+
--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -5,14 +5,14 @@ import Popular from "../../components/Popular/Popular";
 import Banner from "../../components/Banner/Banner";
 import Video from "../../components/Video/Video";
 import SecondHeader from "../../components/SecondHeader/SecondHeader";
-
-
+import Action from "../../components/Action/Action";
 
 export default function Home() {
   // Initialisation des states
   const [status, setStatus] = useState(false); // State qui donne info si l'utilisateur à cliquer sur films ou séries
   const [uniqueTop, setUniqueTop] = useState([]); // State qui vient ajouter les données d'une seule catégorie
   const [uniqueTendances, setUniqueTendances] = useState([]); // State qui vient ajouter les données d'une seule catégorie
+  const [action, setAction] = useState(null);
   const [blackScreen, setBlackScreen] = useState(false); // State qui permet d'afficher et cacher le popupvideo
   const [idVideo, setIdVideo] = useState(0); // State qui permet de récupérer l'id de la vidéo cliquée
 
@@ -26,6 +26,12 @@ export default function Home() {
   const popularMovies =
     "https://api.themoviedb.org/3/movie/popular?language=en-US&page=1&api_key=aea07ae608264c18c1ea1431604753c3";
 
+  // Actions fetch url
+
+  const theApiKey = "aea07ae608264c18c1ea1431604753c3";
+  const actionMoviesFetchURL = `https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc&with_genres=28&api_key=${theApiKey}`;
+  const actionSeriesFetchURL = `https://api.themoviedb.org/3/discover/tv?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc&with_genres=10759&api_key=${theApiKey} `;
+
   // Comportements
 
   const shuffle = (arr) => [...arr].sort(() => Math.random() - 0.5);
@@ -36,12 +42,23 @@ export default function Home() {
       .then((response) => response.json())
       .then((response) => setUniqueTop(response.results.splice(0, 10)))
       .catch((err) => console.error(err));
+
+      fetch(actionMoviesFetchURL)
+      .then((response) => response.json())
+      .then((data) => setAction(data.results.splice(0, 10)))
+      .catch((err) => console.error(err));
+
     fetch(popularMovies)
       .then((response) => response.json())
       .then((response) =>
         setUniqueTendances(shuffle(response.results).splice(0, 15))
       )
       .catch((err) => console.error(err));
+
+   
+   
+   
+   
   };
 
   const handleSeries = () => {
@@ -50,12 +67,19 @@ export default function Home() {
       .then((response) => response.json())
       .then((response) => setUniqueTop(response.results.splice(0, 10)))
       .catch((err) => console.error(err));
+
+      fetch(actionSeriesFetchURL)
+      .then((response) => response.json())
+      .then((data) => setAction(data.results.splice(0, 10)))
+      .catch((err) => console.error(err));
+
     fetch(popularSeries)
       .then((response) => response.json())
       .then((response) =>
         setUniqueTendances(shuffle(response.results).splice(0, 15))
       )
       .catch((err) => console.error(err));
+   
   };
 
   const handleAll = () => {
@@ -63,7 +87,7 @@ export default function Home() {
   };
 
   return (
-    <section className= "home">
+    <section className="home">
       {blackScreen && (
         <Video idVideo={idVideo} setBlackScreen={setBlackScreen} />
       )}
@@ -78,15 +102,20 @@ export default function Home() {
           Series
         </button>
       </div>
+      <SecondHeader
+        handleAll={handleAll}
+        handleMovies={handleMovies}
+        handleSeries={handleSeries}
+      />
       <Banner setBlackScreen={setBlackScreen} setIdVideo={setIdVideo} />
       <Top10 status={status} uniqueTop={uniqueTop} />
-      <Popular
+    <Popular
         status={status}
         uniqueTendances={uniqueTendances}
         shuffle={shuffle}
-      />
+      /> 
 
-      <SecondHeader/>
+     {action && <Action status={status} actions={action} />}
     </section>
   );
 }
